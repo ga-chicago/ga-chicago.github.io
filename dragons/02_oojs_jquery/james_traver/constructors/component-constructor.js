b9e85576b97bebd369d8e18cc7ae8f087549463c
James Traver <jtraverjr@gmail.com>
2015-11-04 16:45:26 -0600

    2.3 final

---
+++ dragons/02_oojs_jquery/james_traver/constructors/component-constructor.js
@@ -0,0 +1,11 @@
+function component(domElement) {
+
+  this.domElement = domElement;
+  this.initialize = function() {
+    console.log('init');
+  };
+  this.render = function() {
+    console.log('render')
+  }
+
+}
---
+++ dragons/02_oojs_jquery/james_traver/constructors/turtles.js
@@ -0,0 +1,17 @@
+
+// 1. create a constructor for ninja turtles
+// 2. ask for arguments: 'name', 'colour', 'weapon', 'favouritePizzaType'
+// 3. implement our constructor
+// 4. create an instance of turtles for each of the 4 ninja turtles
+// 5. test this in the console of your choice!
+
+function turtle() {
+
+}
+
+var donatello = new turtle();
+
+// 6. repeat the above process for lego characters
+// 7. special individual heads
+// 8. special colors/clothes
+// 9. accessories
